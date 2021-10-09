8aaf5c47e9df82e2098cec95bdf26bbb504c28e4
Vishnu <vprabh@pdisoftware.com>
2021-10-09 12:37:59 -0500

    done

--- wk4-day3-assignment/registration-form/start.js
+++ wk4-day3-assignment/registration-form/start.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
-mongoose.connection.on('open', () => {
-    console.log('Mongoose connection open');
+mongoose.connection
+  .on("open", () => {
+    console.log("Mongoose connection open");
   })
-  .on('error', (err) => {
+  .on("error", (err) => {
     console.log(`Connection error: ${err.message}`);
   });
 
-require('./models/Registration.js');
+require("./models/Registration.js");
 
-const app = require('./app.js');
+const app = require("./app.js");
 
 const server = app.listen(3000, () => {
   console.log(`Express is running on port ${server.address().port}`);
